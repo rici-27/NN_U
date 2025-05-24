2c6cc54296ca8007b11c88f288679b36f1372b4d
rici-27 <ricarda.buttmann@icloud.com>
2025-05-24 18:16:53 +0200

    bye

--- main.py
+++ main.py
@@ -3,52 +3,70 @@ from network import *
 from abc import ABC
 import numpy as np
 import time
-import keras  
+import keras
 from optimizer import SGDTrainer
 from layer import *
 import argparse
 
-def run_model(folder_path, train = True):
 
-    # Hier werden nun die verschiedenen Layer angelegt, input und loss layer werden separat behandelt
-    # S: das könnten wir vielleicht mit so einer config.tx file machen, dann ist das ganze etwas einfach
-    # bzw. dann müssen wir nicht jedes Layer einzeln einfügen
+def run_model(folder_path, train=True):
+
+    # Layer für das Netzwerk erstellen
 
     input_layer = Input_Layer_MNIST(784)
-    loss_layer = Cross_Entropy_Loss_Layer() # auf random
-
+    loss_layer = Cross_Entropy_Loss_Layer()  # auf random
     layers = []
-    layers.append(FCN_Layer(inShape = 784, outShape = 196, num = 1))
-    layers.append(ACT_Layer_sigmoid(inShape = 196))
-    layers.append(FCN_Layer(inShape = 196, outShape = 98, num = 2))
-    layers.append(ACT_Layer_tanH(inShape = 98))
-    layers.append(FCN_Layer(inShape = 98, outShape = 10, num = 3))
-    layers.append(Softmax_Layer(inShape = 10))
+
+    # Config file einlesen
+
+    # Abkürzungen für die config Datei
+
+    class_map = {
+        "fcn": FCN_Layer,
+        "sigmoid": ACT_Layer_sigmoid,
+        "tanh": ACT_Layer_tanH,
+        "relu": ACT_Layer_ReLu,
+        "softmax": Softmax_Layer,
+
+    }
+
+    # Datei einlesen und Objekte erzeugen
+
+    with open("config.txt", "r") as file:
+        for line in file:
+            parts = [part.strip() for part in line.strip().split(",")]
+            class_name = parts[0]
+            args = list(map(int, parts[1:]))
+
+            if class_name in class_map:
+                obj = class_map[class_name](*args)
+                layers.append(obj)
+            else:
+                print(f"Unbekannte Klasse: {class_name}")
+
 
     # Netzwerk definieren
-    network_mnist = Network(input_layer = input_layer, layers = layers, loss_layer = loss_layer)
+    network_mnist = Network(input_layer=input_layer, layers=layers, loss_layer=loss_layer)
 
     # Testdaten werden geladen und in ein passendes Format gebracht. /255 aufgrund der Graustufen
-    # --> x_test sollen floats zwischen 0 und 1 sein
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-    x_train = x_train /255
-    x_test = x_test /255
+    x_train = x_train / 255
+    x_test = x_test / 255
 
     # One-Hot-Encoding für Trainingsdata
     y_train_onehot = np.zeros([y_train.size, 10])
     y_train_onehot[np.arange(y_train.size), y_train] = 1
-    
+
     train_data = (x_train, y_train_onehot)
 
     # Netzwerk trainieren bzw. Parameter einlesen
     if train == True:
-        trainer = SGDTrainer(0.001, 20)
+        trainer = SGDTrainer(0.01, 20)
         trainer.optimizing(network_mnist, train_data)
         network_mnist.saveParams(folder_path)
-        
+
     else:
         network_mnist.loadParams(folder_path)
-
 
     # Netzwerk testen
     mistakes = 0
@@ -58,21 +76,14 @@ def run_model(folder_path, train = True):
         pred = np.argmax(network_mnist.tensor_list[-1].elements)
         if pred != y:
             mistakes += 1
-    
-    print("Fehlerquote: ", mistakes/len(y_test))    
-        
-    # hier soll noch was ausgegeben und gespeichert werden
-    
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description="Decide mode for NN and insert folder path for parameter", add_help=False)
-#     parser.add_argument("--train", type=bool, default=True, help="train or just evaluate?")
-#     parser.add_argument("--folder_path", type=str, default = r"C:\Users\Anwender\Desktop\Neuronale Netze", help="insert folder path for parameters")
-#     args = parser.parse_args()
-    
-#     run_model(args.train, args.folder_path) 
 
+    err = mistakes / len(y_test)
 
-folder_path = r"C:\Users\Simon\Desktop\Neuronale Netze"
+    print("Fehlerquote: ", err)
+    print("Accuracy: ", 1-err)
 
+# Hier passenden Ordnerpfad eingeben wie in README beschrieben
+folder_path = r"C:\Users\Anwender\Desktop\Neuronale Netze"
 
+# Zweites Argument (True/False) gibt an, ob der Trainingsmodus aktiviert werden soll
 run_model(folder_path, True)
